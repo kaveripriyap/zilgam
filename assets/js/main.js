187cee8cbbde831c27ab3a24c99688f9542b0f82
Kaveri Priya Putti <kaveripriyap2312@gmail.com>
2021-07-02 20:06:00 +0800

    Add about

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,14 +44,14 @@ sr.reveal('.about__img',{});
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+// /*SCROLL GALLERY*/
+// sr.reveal('.gallery__carousel',{}); 
+// sr.reveal('.gallery__text',{}); 
+// sr.reveal('.gallery__data',{interval: 200}); 
+// sr.reveal('.gallery__img',{delay: 600});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+/*SCROLL MY PIECES*/
+sr.reveal('.mypieces__img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
