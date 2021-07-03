dfd79fc178f5d5b885bef47e429641463fe9c41f
Kaveri Priya Putti <kaveripriyap2312@gmail.com>
2021-07-03 12:55:04 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
+sr.reveal('.home__data',{delay: 200})
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
